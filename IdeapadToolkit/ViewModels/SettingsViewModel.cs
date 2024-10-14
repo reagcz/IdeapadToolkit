0875492582d204047c0ab251f6711507492729c3
Filip Havlíček <philiphavlicek@gmail.com>
2024-10-14 19:46:26 +0200

    Add translation support

--- IdeapadToolkit/ViewModels/SettingsViewModel.cs
+++ IdeapadToolkit/ViewModels/SettingsViewModel.cs
@@ -2,9 +2,16 @@
 using IdeapadToolkit.Services;
 using ModernWpf.Controls;
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows;
 
 namespace IdeapadToolkit.ViewModels
 {
+    public record Locale(string Name, string CultureInfoCode);
 
     [INotifyPropertyChanged]
     public partial class SettingsViewModel
@@ -16,12 +23,14 @@ namespace IdeapadToolkit.ViewModels
             _runOnStartupService = runOnStartupService;
         }
 
+        public Locale[] SupportedLanguages { get; init; } =
+            [new Locale("English", "en-US"),
+            new Locale("Čeština", "cs-CZ"),
+            new Locale("Français", "fr-FR")];
+
         public bool IsRunOnStartupEnabled
         {
-            get
-            {
-                return _runOnStartupService.IsRunOnStartupEnabled();
-            }
+            get => _runOnStartupService.IsRunOnStartupEnabled();
             set
             {
                 try
@@ -36,8 +45,31 @@ namespace IdeapadToolkit.ViewModels
                         Content = ex.Message,
                         CloseButtonText = "Ok"
                     }.ShowAsync();
-                    
                 }
+            }
+        }
+
+        public Locale SelectedLocale
+        {
+            get
+            {
+                var setting = Settings.Default.Language;
+                var locale = SupportedLanguages.FirstOrDefault(x => x.CultureInfoCode == setting);
+                return locale ?? new Locale(String.Empty, String.Empty);
+            }
+            set
+            {
+                if (String.IsNullOrEmpty(value?.Name))
+                    return;
+
+                Settings.Default.Language = value.CultureInfoCode;
+                var culture = CultureInfo.GetCultureInfo(value.CultureInfoCode);
+                if (culture != null)
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        Thread.CurrentThread.CurrentUICulture = culture;
+                    });
+                Settings.Default.Save();
             }
         }
     }
